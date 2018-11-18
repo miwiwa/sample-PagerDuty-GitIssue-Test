83d8b27bc1a955277866d6912b1c951ac16489eb
Michael Walker <miwalker@us.ibm.com>
2018-11-18 07:00:40 +0000

    h

--- wrapper.bash
+++ wrapper.bash
@@ -21,18 +21,18 @@ while read line; do
   	fi
 done <${filename}
 
-executable_script=$1
-incident=$2
-issue=$3
+#executable_script=$1
+#incident=$2
+#issue=$3
 
-echo "executable script: $executable_script"
+#echo "executable script: $executable_script"
 
-qa_test="$(node $executable_script lint)"
+#qa_test="$(node $executable_script lint)"
 
-if [[ $? != 0 ]]
-then
-	/usr/bin/python create_alert.py -a $incident $issue
-else
-	echo "tests passed"
-fi
+#if [[ $? != 0 ]]
+#then
+#	/usr/bin/python create_alert.py -a $incident $issue
+#else
+#	echo "tests passed"
+#fi
 
