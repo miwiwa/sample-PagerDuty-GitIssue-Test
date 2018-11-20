e9b7353a60739ba76a248596736683eeda745c56
Michael Walker <miwalker@us.ibm.com>
2018-11-20 22:19:31 +0000

    Add artifactory call

--- wrapper.bash
+++ wrapper.bash
@@ -5,6 +5,9 @@ enable_alerts=()
 filename="notification.exclude.conf"
 echo "IDS_JOB_NAME: ${IDS_JOB_NAME}"
 
+echo "Downloading tarball from Artifactory"
+$(curl -H 'X-JFrog-Art-Api:AKCp5btVRb4K6PURdgkMNLBEDquhwQmhD1ogoxvybo1ajDYmcDeYuSYQewVTudyMsakEeMPMu' -O "https://na.artifactory.swg-devops.com/artifactory/wh-itops-generic-local/<TARGET_FILE_PATH>"')
+
 var=$(grep $IDS_JOB_NAME $filename | sed 's:[^;]*/\(.*\):\1:')
 echo "var: $var"
 num=$(echo $var | tr -cd ';' | wc -c)
