514c8e479f5c171b40bc55ba285aa36fe4d9e69b
Michael Walker <miwalker@us.ibm.com>
2018-11-20 00:09:09 +0000

    f

--- wrapper.bash
+++ wrapper.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-pwd
-ls -la
+enable_alerts=()
 
 filename="notification.exclude.conf"
 echo "IDS_JOB_NAME: ${IDS_JOB_NAME}"
@@ -11,8 +10,23 @@ echo "var: $var"
 num=$(echo $var | tr -cd ';' | wc -c)
 
 for ((i=2;i<=$num+1;i++)); do
-        echo "\"$var"\ | cut -d ";" -f $i""
+        alert_type=$(echo "\"$var"\ | cut -d ";" -f $i"")
+        enable_alerts+=$alert_type
 done
+
+if [[ " ${enable_alerts[@]} " =~ "no-pagerduty" ]] && [[ " ${enable_alerts[@]} " =~ "no-git" ]] ; then
+    echo "PagerDuty and Git Issues not configured for this job"
+    exit 0
+elif [[ " ${enable_alerts[@]} " =~ "no-pagerduty" ]]; then
+	echo "Creating git issue from wrapper"
+	/usr/bin/python create_alert.py -a $issue
+elif [[ " ${enable_alerts[@]} " =~ "no-git" ]]; then
+	echo "Creating Pager Duty incident from wrapper"
+	/usr/bin/python create_alert.py -a $incident
+else
+	echo "Creating both Pager Duty incident and Git issue from wrapper"
+	/usr/bin/python create_alert.py -a $incident $issue
+fi
 
 #executable_script=$1
 #incident=$2
