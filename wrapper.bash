5449d4b1f94af1ec441d65fafb31d1ddf70b44dd
Michael Walker <miwalker@us.ibm.com>
2018-11-20 00:19:53 +0000

    y

--- wrapper.bash
+++ wrapper.bash
@@ -28,18 +28,3 @@ else
 	/usr/bin/python create_alert.py -a $incident $issue
 fi
 
-#executable_script=$1
-#incident=$2
-#issue=$3
-
-#echo "executable script: $executable_script"
-
-#qa_test="$(node $executable_script lint)"
-
-#if [[ $? != 0 ]]
-#then
-#	/usr/bin/python create_alert.py -a $incident $issue
-#else
-#	echo "tests passed"
-#fi
-
