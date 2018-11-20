9d9626da635bcf1ef3ac786c03786ee81025cefd
Michael Walker <miwalker@us.ibm.com>
2018-11-20 22:51:59 +0000

    removed extra quote

--- wrapper.bash
+++ wrapper.bash
@@ -6,7 +6,7 @@ filename="notification.exclude.conf"
 echo "IDS_JOB_NAME: ${IDS_JOB_NAME}"
 
 echo "Downloading tarball from Artifactory"
-$(curl -H 'X-JFrog-Art-Api:AKCp5btVRb4K6PURdgkMNLBEDquhwQmhD1ogoxvybo1ajDYmcDeYuSYQewVTudyMsakEeMPMu' -O "https://na.artifactory.swg-devops.com/artifactory/wh-itops-generic-local/<TARGET_FILE_PATH>"')
+$(curl -H 'X-JFrog-Art-Api:AKCp5btVRb4K6PURdgkMNLBEDquhwQmhD1ogoxvybo1ajDYmcDeYuSYQewVTudyMsakEeMPMu' -O "https://na.artifactory.swg-devops.com/artifactory/wh-itops-generic-local/<TARGET_FILE_PATH>")
 
 var=$(grep $IDS_JOB_NAME $filename | sed 's:[^;]*/\(.*\):\1:')
 echo "var: $var"
