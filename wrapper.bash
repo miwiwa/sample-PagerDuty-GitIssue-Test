afe6faf1080c645bd2fedc7ccbe0e72bca24e1e9
Michael Walker <miwalker@us.ibm.com>
2018-11-20 23:33:14 +0000

    g

--- wrapper.bash
+++ wrapper.bash
@@ -6,7 +6,7 @@ filename="notification.exclude.conf"
 echo "IDS_JOB_NAME: ${IDS_JOB_NAME}"
 
 echo "Downloading tarball from Artifactory"
-$(curl -H 'X-JFrog-Art-Api:AKCp5btVRb4K6PURdgkMNLBEDquhwQmhD1ogoxvybo1ajDYmcDeYuSYQewVTudyMsakEeMPMu' -O "https://na.artifactory.swg-devops.com/artifactory/wh-itops-generic-local/<TARGET_FILE_PATH>")
+$(curl -H 'X-JFrog-Art-Api:${artifactory_apikey}' -O "https://na.artifactory.swg-devops.com/artifactory/wh-itops-rhel-7-supplementary-rpm-local/https://na.artifactory.swg-devops.com:443/artifactory/wh-itops-rhel-7-supplementary-rpm-local/OVMF-20160608b-1.git988715a.el7.noarch.rpm")
 
 var=$(grep $IDS_JOB_NAME $filename | sed 's:[^;]*/\(.*\):\1:')
 echo "var: $var"
