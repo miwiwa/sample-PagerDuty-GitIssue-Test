896bfa4aedcbf74dc2a2cdaf799b6e796a368eda
Michael Walker <miwalker@us.ibm.com>
2018-11-21 19:22:04 +0000

    remove echos and rename file

--- send_alert.bash
+++ send_alert.bash
@@ -1,19 +1,25 @@
 #!/bin/bash
+#
+#  Sends alert with link to failing job to Pager Duty or Git Issue depending on pipeline config
+#
 
 enable_alerts=()
 
+# exclude file contains list of alerts not to send
 filename="notification.exclude.conf"
-echo "IDS_JOB_NAME: ${IDS_JOB_NAME}"
 
+# Retrieve line from exclusion list for current job
 var=$(grep $IDS_JOB_NAME $filename | sed 's:[^;]*/\(.*\):\1:')
-echo "var: $var"
-num=$(echo $var | tr -cd ';' | wc -c)
 
+#num=$(echo $var | tr -cd ';' | wc -c)
+
+# Loop through line and add exclusion to array
 for ((i=2;i<=$num+1;i++)); do
         alert_type=$(echo "\"$var"\ | cut -d ";" -f $i"")
         enable_alerts+=$alert_type
 done
 
+# Call python script to send alerts based on content of array
 if [[ " ${enable_alerts[@]} " =~ "no-pagerduty" ]] && [[ " ${enable_alerts[@]} " =~ "no-git" ]] ; then
     echo "PagerDuty and Git Issues not configured for this job"
     exit 0
