90638a825cabc94f8d06eb3e82c0ae33808b957c
Michael Walker <miwalker@us.ibm.com>
2019-01-18 08:33:04 +0000

    g

--- send_alert.bash
+++ send_alert.bash
@@ -32,7 +32,7 @@ echo "var: $var"
 
 num=$(echo $var | tr -cd ';' | wc -c)
 echo "num: $num"
-# Loop through line and add exclusion to array
+# Loop throughr line and add exclusion to array
 for ((i=2;i<=$num+1;i++)); do
        alert_type=$(echo "\"$var"\ | cut -d ";" -f $i"")
        
