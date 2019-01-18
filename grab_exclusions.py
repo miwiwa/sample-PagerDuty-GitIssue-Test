af60abd40c4bf1ff03109d9c066178d82e92889e
Michael Walker <miwalker@us.ibm.com>
2019-01-18 00:46:50 +0000

    j

--- grab_exclusions.py
+++ grab_exclusions.py
@@ -62,8 +62,8 @@ def get_config_value(data, target):
             yield value    
 
 def main():
-    for x in get_config_value(pipeline_config, param_value):
-        print(x)
+    config_value = get_config_value(pipeline_config, param_value):
+        print(config_values)
         
 if __name__ == '__main__':
     main()
