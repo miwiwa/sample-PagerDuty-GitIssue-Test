26f15ed4bab99af1b80017cf9e2b43497c3defd8
Michael Walker <miwalker@us.ibm.com>
2019-01-22 17:59:47 +0000

    k

--- create_alert.py
+++ create_alert.py
@@ -23,6 +23,12 @@ description = 'Specify creation of incident/issue in Pagerduty and Git Issues'
 parser = argparse.ArgumentParser(     description=__doc__)
 parser.add_argument('-a', '--ALERTS', nargs='+', type=str.lower, dest='ALERTS', help="Enter 'incident', 'issue', and/or 'message' to send info to PagerDuty, Git, or Slack", required=True)
 parser.add_argument('-s', '--STATUS', nargs='?', type=str.lower, dest='STATUS', default='Executed', help="Enter 'started' or 'completed' for Slack alerts")
+#additional params not needed by this script but by other
+parser.add_argument('-c', '--CONFIG', nargs='?', type=str.lower, dest='CONFIG', help="Enter name of config file to search")
+parser.add_argument('-e', '--EXCLUSIONS', nargs='?', type=str.upper, default="ALERT_EXCLUSIONS", dest='EXCLUSIONS', help="Enter name of parameter to retrieve")
+parser.add_argument('-d', '--VALUE', nargs='?', type=str.upper, dest='VALUE', help="Enter name of parameter to retrieve")
+parser.add_argument('-z', '--zzzzz', dest='ZZZZZ', action='store_true')
+
 args = parser.parse_args()
 alerts = args.ALERTS
 job_status = args.STATUS
