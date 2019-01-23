76fcd4c338175b862bfada90dd5e7da9670d1520
Michael Walker <miwalker@us.ibm.com>
2019-01-23 02:05:01 +0000

    n

--- create_alert.py
+++ create_alert.py
@@ -176,6 +176,19 @@ def trigger_slackMessage():
     }
     d = {}
     
+    print("Checking Slack parameters in toolchain.json")
+    # Parse dict for PagerDuty parameters
+    try:
+        slack_service_id = [i['parameters']['service_id'] for i in data["services"] if 'slack' in i['broker_id']]
+        slack_api_token = [i['parameters']['api_token'] for i in data["services"] if 'slack' in i['broker_id']]
+        sl_api_token = slack_api_token[0]
+        sl_service_id = slack_service_id[0]
+        print("sl_api_token:", sl_api_token)
+        print("sl_service_id:", sl_service_id)
+    except (KeyError, IndexError):
+        print("ERROR: Slack is not configured with the toolchain")
+        return 1
+        
     if job_status == 'started':
         d['text'] = "Job *" + ids_job_name + "* in Stage *" + ids_stage_name + "* : *" + ids_stage_num + "* " + job_status + "\n Triggered by: " + trigger_user + "\n Started at: " + current_time
     elif job_status == 'success':
