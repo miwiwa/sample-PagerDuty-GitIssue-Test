0df19f6bfdd0c86b6a46a4bee6e3f9268ad28087
Michael Walker <miwalker@us.ibm.com>
2019-01-28 05:29:11 +0000

    j

--- send_alert.bash
+++ send_alert.bash
@@ -24,7 +24,7 @@ curl -sSL -u "watkins0@us.ibm.com:${gitApiKey}" "https://raw.github.ibm.com/whc-
 
 #get_exclusions=$(python pipeline.py -c $filename -d $alert_exclusions -e)
 get_exclusions=$(python -c 'import pipeline; pipeline.get_job_exclusions("pipeline.config", "ALERT_EXCLUSIONS", "Linting")')
-get_namespace=$(python -c 'import pipeline; pipeline.retrieve_config_value($filename, "POLICY_NAME")')
+get_namespace=$(python -c 'import pipeline; pipeline.retrieve_config_value("$filename", "POLICY_NAME")')
 
 echo "get_namespace: $get_namespace"
 echo "get_exclusions: $get_exclusions"
